c7fac8796eb61a46bd4eae6091b245abe14619c6
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-08-25 13:34:53 +0530

    Update storage5.sol

--- contracts/storage5.sol
+++ contracts/storage5.sol
@@ -12,15 +12,5 @@ contract storageSpace {
         }
     }
 
-    //solution
 
-    function sol() external {
-        uint256 length = arr.length; // one state read
-        uint256 local_mycounter = counter; // one state read
-        for (uint256 i; i < length; i++) {
-            // local reads
-            local_mycounter++; // local reads and writes
-        }
-        counter = local_mycounter; // one state write
-    }
 }
