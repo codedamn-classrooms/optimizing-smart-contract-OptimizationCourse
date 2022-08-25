3fca88158becf694902e1dd7ca59c59cb0f00801
Ksh <ksrivas199@gmail.com>
2022-08-25 15:10:19 +0530

    optimize14

---
+++ contracts/optimize14.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract optimize {
+    function blue() external pure returns (uint256) {
+        return 1;
+    }
+
+    function white() external pure returns (uint256) {
+        return 1;
+    }
+
+    function green() external pure returns (uint256) {
+        return 1;
+    }
+}
